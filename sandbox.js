4fe655ca43e399f7a97f190bf8ed070347d41a64
cybort360 <cybort360@gmail.com>
2025-05-03 12:29:06 +0100

    twNormie Giveaway

---
+++ sandbox.js
@@ -0,0 +1,61 @@
+'use strict';
+
+const username = [
+  '@theProcessXCII',
+  'obottweets',
+  'Muh__Toyeeb',
+  'Aunter07',
+  'HundredX100X',
+  'Thebetaone',
+  'Dominione13',
+  'kali4kt_eth',
+  'Deymar_Michael',
+  'agba_merchant',
+  'Blacksa93253702',
+  'heis_the_law',
+  'mina4ty',
+  'd_gback',
+  'trollgidget',
+  'Robertdivine8',
+  'davidrules17',
+  '99nevey',
+  'DegenUnknown',
+  'Folarihn_',
+  'zeusonearthx',
+  'Emmy90_ox',
+  '_RISWAN0',
+  'Danni_Defi',
+  'Allen_DIP',
+  'nigglesniggles',
+  'Eldivine_solace',
+  'nnachieke',
+  'Kennydgoldenboy',
+  'OfficialFlokiy',
+  'darkigbogirl',
+  'secularre',
+  'ardemorla',
+  'missoasiss',
+  '___dirtydee',
+  'Fw_j3ff',
+  'KimLongmeme',
+  'thekni9htking',
+  'Dahgirl_',
+  'paulothefirst',
+  'Tallsome_',
+  'eeagbi',
+  '_thanosonuche',
+  'pleasestopppfr',
+  'maira1359491',
+  'ikejijbrim',
+  'kolisnoj17070',
+  'of_ct49678',
+  'Hardarmthebull',
+];
+const getWinner = function (arr) {
+  const randomUsername = Math.floor(Math.random() * arr.length);
+  alert(arr[randomUsername]);
+};
+
+getWinner(username);
+
+// console.log(Math.random() * username);
